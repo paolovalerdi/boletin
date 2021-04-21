760c38e9089ed4429d898be7a6ca4057bb3c4c36
Paolo Valerdi <paolovalerdi@gmail.com>
2021-04-21 12:02:08 -0500

    Add full article model and create the model folder

---
+++ lib/model/article.dart
@@ -0,0 +1,17 @@
+class Article {
+  final String url;
+  final String title;
+  final String body;
+  final String date;
+  final String category;
+  final List<String> images;
+
+  Article(
+    this.url,
+    this.title,
+    this.body,
+    this.date,
+    this.category,
+    this.images,
+  );
+}
